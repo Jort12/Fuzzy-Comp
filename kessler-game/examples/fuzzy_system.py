5da2c153124c4f579ae6c241bb592828ccd867f1
Kyle Nguyen <kyle.nguyen@digipen.edu>
2025-10-02 12:09:20 -0700

    WIP sugeno eval

--- kessler-game/examples/fuzzy_system.py
+++ kessler-game/examples/fuzzy_system.py
@@ -7,9 +7,21 @@ import math
 
 
 class SugenoRule:
-    def __init__(self, antecedents, consequents, weight=1.0):
-        self.antecedents = antecedents  #list of (fuzzy_set_name, membership_value) tuples
-        self.consequents = consequents  #list of (output_name, output_value) tuples
+    def __init__(self, antecedents_func, consequents_func, weight=1.0):
+        self.antecedents = antecedents_func  #list of (fuzzy_set_name, membership_value) tuples
+        self.consequents = consequents_func  #list of (output_name, output_value) tuples
         self.weight = weight  #weight of the rule, default to 1.0
-    def evaluate(self, input_values):
+    def fire(self, context):#Context is everything we know about the ship and game state
+        mu = max(0, min(1, self.antecedents(context))) * self.weight
+        if mu ==0:
+            return 0, {}
+        return mu, self.consequents(context)
 
+
+def eval_Sugeno(rules, context): 
+    #SUM of weighted outputs(dot product) divided by SUM of weights (dot product)
+    outputs = {}
+    weights = {}
+    for rule in rules:
+
+        
