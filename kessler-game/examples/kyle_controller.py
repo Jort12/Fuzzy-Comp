f03ec2decc65d6333accbf548b434b218a833c5a
thuykhanh2642 <thuykhanh2642@gmail.com>
2025-10-01 20:22:34 -0700

    init full fuzzy design

---
+++ kessler-game/examples/kyle_controller.py
@@ -0,0 +1,7 @@
+#Author: Kyle Nguyen
+#Description: A full fuzzy logic controller for the Kessler game.
+
+from kesslergame import Controller
+from util import wrap180, intercept_point, side_score, triag
+import math
+
