ea960c348e03989e6ad40bf9499c594057c3f066
thuykhanh2642 <thuykhanh2642@gmail.com>
2025-10-06 21:51:16 -0700

    WIP FIS

--- kessler-game/examples/fuzzy_system.py
+++ kessler-game/examples/fuzzy_system.py
@@ -40,13 +40,27 @@ class SugenoSystem:
         self.rules.append(rule)
     def evaluate(self,inputs:dict): #evalutate with crisp inputs, ex:{'dist': 300, 'approach': 1.5, 'ammo': 3}
         numerator, denominator = 0.0,0.0
-        
+        #For each rule, calculate its strength and contribute to the output
+        for rule in self.rules:
+            mus = []
+        ###WIP
         
 
+
+
+#WIP
 class MamdaniRule:
     def __init__(self, antecedents, consequents, weight=1.0):
         self.antecedents = antecedents  #list of (fuzzy_set_name, membership_value) tuples
         self.consequents = consequents  #list of (output_name, fuzzy_set_name) tuples
         self.weight = weight  #weight of the rule, default to 1.0
+class MamdaniSystem:
+    def __init__(self, rules=None, mode="prod"):
+        self.rules = rules if rules else []
+        self.mode = mode  #"prod" or "min"
+    def add_rule(self,rule:MamdaniRule):
+        self.rules.append(rule)
+    def evaluate(self,inputs:dict):
+        pass
         
         
