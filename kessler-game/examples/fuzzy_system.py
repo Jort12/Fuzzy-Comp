9201e6cec48bfaae9865b31f58731f812336a627
thuykhanh2642 <thuykhanh2642@gmail.com>
2025-10-06 20:56:22 -0700

    Refactor

--- kessler-game/examples/fuzzy_system.py
+++ kessler-game/examples/fuzzy_system.py
@@ -1,49 +1,23 @@
 #Author: Kyle Nguyen
 #Description: Everything needed for fuzzy logic controller
 from kesslergame.controller import KesslerController
-from util import wrap180, intercept_point, side_score, triag, trap
-import math
+from util import wrap180, intercept_point, side_score
+
 
 
 
 class SugenoRule:
-    def __init__(self, antecedents_func, consequents_func, weight=1.0):
-        self.antecedents = antecedents_func  #list of (fuzzy_set_name, membership_value) tuples
-        self.consequents = consequents_func  #list of (output_name, output_value) tuples
+    def __init__(self, antecedents, consequents, weight=1.0):
+        self.antecedents = antecedents  #list of (fuzzy_set_name, membership_value) tuples
+        self.consequents = consequents  #list of (output_name, output_value) tuples
         self.weight = weight  #weight of the rule, default to 1.0
-    def fire(self, context):#Context is everything we know about the ship and game state
-        mu = max(0, min(1, self.antecedents(context))) * self.weight
-        if mu ==0:
-            return 0, {}
-        return mu, self.consequents(context)
-
-
-def eval_Sugeno(rules, context):  #Return a value 0-1
-    #SUM of weighted outputs(dot product) divided by SUM of weights (dot product)
-    outputs = {}
-    weights = {}
-    for rule in rules:
-        mu, consequents = rule.fire(context)
-        if mu<=0: continue
-
-        for output_name, output_value in consequents.items():
-            if output_name not in outputs:
-                outputs[output_name] = 0.0
-                weights[output_name] = 0.0
-            
-            outputs[output_name] += mu * output_value
-            weights[output_name] += mu
-        
-    result = {}
-    for output_name in outputs:
-        if weights[output_name] == 0:
-            result[output_name] = 0
-        else:
-            result[output_name] = outputs[output_name] / weights[output_name]
-
-    return result
-    
-            
     
 
+
+
+class MamdaniRule:
+    def __init__(self, antecedents, consequents, weight=1.0):
+        self.antecedents = antecedents  #list of (fuzzy_set_name, membership_value) tuples
+        self.consequents = consequents  #list of (output_name, fuzzy_set_name) tuples
+        self.weight = weight  #weight of the rule, default to 1.0
         
