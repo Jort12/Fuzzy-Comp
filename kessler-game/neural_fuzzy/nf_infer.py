d7280df62a1a3d443d92e4944979d834460d77c8
Kyle Nguyen <kyle.nguyen@digipen.edu>
2025-10-23 17:34:27 -0700

    added sugenonetwork

---
+++ kessler-game/neural_fuzzy/nf_infer.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset, random_split
--- kessler-game/neural_fuzzy/sugeno_nn.py
+++ kessler-game/neural_fuzzy/sugeno_nn.py
@@ -7,7 +7,10 @@ import itertools
 """
 Gaussian Membership Function Layer:
     Purpose: To compute the degree of membership of inputs to fuzzy sets using Gaussian functions.
-Sugeno FIS Layer: tweak the sigma and the center values
+
+RuleLayer:
+    Purpose: To compute the firing strength of fuzzy rules based on the membership degrees from the MF layer.
+Sugeno Layer: tweak the sigma and the center values
 
     
 
@@ -58,3 +61,49 @@ class RuleLayer(nn.Module):
             rule_strengths.append(weight)
 
         return torch.stack(rule_strengths, dim=1) #(batch_size, num_rules)
+    
+
+class SugenoLayer(nn.Module):
+    def __init__(self, num_rules, num_inputs):
+        super().__init__()
+        self.num_rules = num_rules
+        self.num_inputs = num_inputs
+
+        #Consequent parameters: each rule has a linear function of inputs + bias
+        self.consequents = nn.Parameter(torch.randn(num_rules, num_inputs + 1)) #+1 for bias term
+
+
+
+    #rule_strengths: (batch_size, num_rules)
+    #inputs: (batch_size, num_inputs)
+    def forward(self, rule_strengths, inputs):
+        batch_size = inputs.shape[0]
+
+        #bias term to inputs
+        inputs_with_bias = torch.cat([inputs, torch.ones(batch_size, 1, device=inputs.device)], dim=1) #(batch_size, num_inputs + 1)
+
+        #Compute rule outputs. matmul returns the product of 2 tensors
+        rule_outputs = torch.matmul(inputs_with_bias.unsqueeze(1), self.consequents.unsqueeze(0).transpose(1,2)).squeeze(1) #(batch_size, num_rules)
+
+        #Weighted sum of rule outputs
+        weighted_outputs = rule_strengths * rule_outputs #(batch_size, num_rules)
+        output = torch.sum(weighted_outputs, dim=1) / (torch.sum(rule_strengths, dim=1) + 1e-6) #(batch_size,)
+
+        return output
+    
+
+class SugenoNet(nn.Module):
+    def __init__(self, num_inputs, num_mfs, num_outputs):
+        super().__init__()
+        #module lists for MF layers
+        self.mf_layers = nn.ModuleList([
+            GaussianMF(f"input_{i}", num_mfs, [0.3, 0.5, 0.7]) for i in range(num_inputs)
+        ])
+        self.rule_layer = RuleLayer(num_inputs, num_mfs)#number of rules = num_mfs^num_inputs
+        self.sugeno_layer = SugenoLayer(num_rules=num_mfs ** num_inputs, num_inputs=num_inputs)
+
+    def forward(self, x):
+        mf_outputs = [mf(x[:, i]) for i, mf in enumerate(self.mf_layers)]#get MF outputs for each input
+        rule_strengths = self.rule_layer(mf_outputs)#get rule strengths
+        y = self.sugeno_layer(rule_strengths, x)#get final output
+        return y
