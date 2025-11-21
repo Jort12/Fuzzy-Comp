496cc9b2ca24ef93e708ec91892ec38af7a25975
Jort12 <ortegagolf@outlook.com>
2025-11-20 16:12:28 -0800

    Have implemented 90% of the Actor

--- kessler-game/examples/FuzzyAcotr_Critic_Agro.py
+++ kessler-game/examples/FuzzyAcotr_Critic_Agro.py
@@ -17,18 +17,24 @@ class FuzzyCritic:
         self.weights += self.learning_rate *td_error*phi_s
 
         return td_error
-
 class FuzzyActor:
-    def __init__(self):
-        pass
-    def act(ship_state, game_state):
-        pass
-    def get_state_features(inputs):
-        pass
-    def update(td_error, state_features):
-        pass
+    def __init__(self, fuzzy_controller):
+        self.controller = fuzzy_controller
+        self.rule_weights = np.ones(len(fuzzy_controller.ctrl_system.rules))
+        
+    def get_state_features(self, inputs):
+        return np.array(list(inputs.values()))
+
+    def act(self, ship_state, game_state):
+        return self.controller.actions(ship_state, game_state)
+    
+    def update(self, td_error, state_features, alpha = 0.01):
+        self.rule_weights += alpha * td_error * state_features
 class FuzzyActorCriticAgent:
-    def __init__(self):
-        pass
+    def __init__(self, actor, critic, gamma = 0.99):
+        self.actor = actor
+        self.critic = critic
+        self.gamma = gamma
+
     def step(state_inputs, ship_state, game_state, reward, next_state_inputs, done):
-        pass
+        
