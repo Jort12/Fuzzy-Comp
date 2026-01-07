f8784f3e3c1f462e31f6438bea9dfd5df209d126
Jort12 <ortegagolf@outlook.com>
2026-01-06 16:52:14 -0800

    restarting for my work

--- kessler-game/examples/ActorCriticAgro/ActorCritic.py
+++ kessler-game/examples/ActorCriticAgro/ActorCritic.py
@@ -1,37 +1,25 @@
-import torch 
-import torch.nn as nn
-import torch.nn.functional as F
+from fuzzy_aggressive_controller import AggressiveFuzzyController
 
-class Actor(nn.Module):
-    def __init__(self, num_rules, action_dim = 4):
-        super().__init__()
-        self.fc1 = nn.Linear(num_rules, 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.out = nn.Linear(128, action_dim)
-
-    def foward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return torch.tanh(self.out(x))
+class Actor:
+    def __init__(self):
+        self.policy = AggressiveFuzzyController()
     
-class Critic(nn.Module):
-    def __init__(self, num_rules):
-        super().__init__()
-        self.fc1 = nn.Linear(num_rules, 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.out = nn.Linear(128, 1)
-
-    def foward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return self.out(x)
+    def act(self, state):
+        return self.policy.act(state)
     
-class FuzzyActorController:
-    def __init__(self, fuzzy_controller, actor: Actor, blend=3.0, device='cpu'):
-        self.fuzzy = fuzzy_controller
-        self.actor = actor.to(device)
-        self.blend = blend
-        self.device = device 
-        
-    def get_action(self, ship_state, game_state, return_mu=False):
-        pass
+    def update(self, advantage):
+        return self.policy.update(advantage)
+    
+class Critic:
+    def __init__(self, alpha=0.1, gamma=0.99):
+        self.w = 0.0
+        self.alpha = alpha
+        self.gamma = gamma
+    
+    def value(self, state):
+        return self.w * state
+    
+    def update(self, reward, state, next_state):
+        td = reward + self.gamma * self.value(next_state) - self.value(state)
+        self.w += self.lr * td * state
+        return td
